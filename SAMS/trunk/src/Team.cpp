6eb089750c1c0f24080c2902e568e5c3d2f2e95c
Kurt.W.Andrews@gmail.com <Kurt.W.Andrews@gmail.com@cc1695ee-5fec-f8fc-3943-34bcf89d173e>
2012-01-22 21:24:53 +0000

    add team stubs

---
+++ SAMS/trunk/src/Team.cpp
@@ -0,0 +1,18 @@
+/*
+ * Team.cpp
+ *
+ *  Created on: Jan 20, 2012
+ *      Author: kandrews
+ */
+
+#include "Team.h"
+
+Team::Team() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Team::~Team() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ SAMS/trunk/src/Team.h
@@ -0,0 +1,17 @@
+/*
+ * Team.h
+ *
+ *  Created on: Jan 20, 2012
+ *      Author: kandrews
+ */
+
+#ifndef TEAM_H_
+#define TEAM_H_
+
+class Team {
+public:
+	Team();
+	virtual ~Team();
+};
+
+#endif /* TEAM_H_ */
